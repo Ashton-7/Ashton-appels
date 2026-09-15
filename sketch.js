16bedb7473e1950d898cf04b514d938a6dafeef3
Ashton-7 <113877@emmauscollege.nl>
2026-09-15 08:29:23 +0000

    new

--- sketch.js
+++ sketch.js
@@ -15,13 +15,13 @@
 let score = 0;
 let appleX = 600; // x-positie van appel 
 let appleY = 50;  // y-positie van appel 
-let appleY2 = 40; 
-let appleX2 = 400; 
-let appleWidth = 20; 
-let basketX = 500; 
-let basketY = 600; 
-let basketWidth = 130; 
-let basketHeight = 60; 
+let appleY2 = 40;
+let appleX2 = 400;
+let appleWidth = 20;
+let basketX = 500;
+let basketY = 600;
+let basketWidth = 130;
+let basketHeight = 60;
 
 /* ********************************************* */
 /* setup() en draw() functies / hoofdprogramma   */
@@ -32,62 +32,62 @@ let basketHeight = 60;
  * de code in deze functie wordt één keer uitgevoerd door 
  * de p5 library, zodra het spel geladen is in de browser 
  */
-function setup() { 
+function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen 
-  createCanvas(1280, 720); 
-} 
+  createCanvas(1280, 720);
+}
 
 /** 
  * draw 
  * de code in deze functie wordt 50 keer per seconde 
  * uitgevoerd door de p5 library, nadat de setup functie klaar is 
  */
-function draw() { 
+function draw() {
   // teken achtergrond 
-  background (39, 183, 240); 
+  background(39, 183, 240);
   /**score */
-fill(255);
-textSize(30);
-text("Score: " + score, 20, 40);
+  fill(255);
+  textSize(30);
+  text("Score: " + score, 20, 40);
 
   // teken appel 
   noStroke();         // geen lijntje om de vorm heen 
   fill(255, 0, 0)     // vulkleur wordt rood 
-  ellipse(appleX, appleY, appleWidth, appleWidth); 
-  appleY = appleY + 2 
+  ellipse(appleX, appleY, appleWidth, appleWidth);
+  appleY = appleY + 2
 
-appleY = appleY + 2;
+  appleY = appleY + 2;
 
-if (appleY > height) {
-  appleY = 0;
-  appleX = random(0, width);
-  score = score - 1;
-}
+  if (appleY > height) {
+    appleY = 0;
+    appleX = random(0, width);
+    score = score - 1;
+  }
 
   fill(210, 0, 0)     // vulkleur wordt rood 
   ellipse(appleX2, appleY2, appleWidth, appleWidth)
-  appleY2 = appleY2 + 4 
+  appleY2 = appleY2 + 4
 
-if (appleY2 > height) {
-  appleY2 = 0;
-  appleX2 = random(0, width);
-  score = score - 1;
-}
+  if (appleY2 > height) {
+    appleY2 = 0;
+    appleX2 = random(0, width);
+    score = score - 1;
+  }
 
   // teken mand 
   noStroke();         // geen lijntje om de vorm heen 
   fill(223, 224, 161)     // vulkleur wordt rood 
-  rect(basketX, basketY, basketWidth, basketHeight); 
+  rect(basketX, basketY, basketWidth, basketHeight);
 
-  basketX = mouseX 
+  basketX = mouseX
 
   if (basketX < 0) {
-  basketX = 0;
-}
+    basketX = 0;
+  }
 
-if (basketX + basketWidth > width) {
-  basketX = width - basketWidth;
-}
+  if (basketX + basketWidth > width) {
+    basketX = width - basketWidth;
+  }
 
   // eerste appel wordt gevangen
   if (
